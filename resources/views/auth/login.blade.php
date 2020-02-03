008b4e046b3660699f19c7507cce8baecd8ee225
RikuHirose <rikuparkour9@gmail.com>
2020-02-03 23:49:16 +0900

    create-auth-form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="p-auth">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto w-100">
+          <div class="card-signin">
+            <h2 class="text-center my-4 black font-weight-bold">ようこそ<span class="light-blue">{{ config('app.name') }}</span>へ</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+            <div class="signup card-body">
+              <h5
+                v-if="callbackMessage"
+                class="title title-up text-center mb-3 font-weight-bold gray">
+                ss
+              </h5>
+              <!-- login fb -->
+              <div>
+                <div class="text-center mb-3">
+                  <button class="m-btn mx-auto col-md-6" btn-type="facebook" type="submit">
+                    facebookアカウントでログイン
+                  </button>
                 </div>
+
+                <div class="mt-2 mb-3 gray text-center">
+                  * SNSに利用状況等が投稿されることはありません
+                </div>
+
+                <div class="or">
+                  <div class="or__block or__block--left"></div>
+                  <div class="or__block or__block--center">
+                    <span>または</span>
+                  </div>
+                  <div class="or__block or__block--right"></div>
+                </div>
+              </div>
+
+              <!-- email signup -->
+              <div>
+                <h5 class="title title-up text-center mb-3 font-weight-bold gray">
+                  メールアドレスでログイン
+                </h5>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                  <label>
+                    名前
+                  </label>
+                  <input
+                    name="name"
+                    class="form-control"
+                    type="email"
+                    placeholder="名前を入力してください"
+                    required>
+                </div>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                  <label>
+                    メールアドレス
+                  </label>
+                  <input
+                    name="email"
+                    class="form-control"
+                    type="email"
+                    placeholder="メールアドレスを入力してください"
+                    required>
+                </div>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                   <label>
+                      パスワード
+                    </label>
+                  <input
+                    name="password"
+                    class="form-control"
+                    type="password"
+                    placeholder="パスワードを入力してください"
+                    required>
+                </div>
+
+                <div class="text-center">
+                  <button
+                    class="m-btn mx-auto col-md-4 my-2" btn-type="primary" type="submit">
+                    メールアドレスでログイン
+                  </button>
+                </div>
+              </div>
+
+              <div class="text-center gray">
+                  アカウントをお持ちでない方は<a href="/register">会員登録</a>
+              </div>
             </div>
+            <div class="footer text-center">
+              <div class="gray">
+                {{ config('app.name') }}の登録は、<a href="/terms" target="_blank">利用規約 </a>および<a href="/privacy" target="_blank">プライバシーポリシー </a>に同意したものとみなします。
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="p-auth">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto w-100">
+          <div class="card-signin">
+            <h2 class="text-center my-4 black font-weight-bold">ようこそ<span class="light-blue">{{ config('app.name') }}</span>へ</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div class="signup card-body">
+              <h5
+                v-if="callbackMessage"
+                class="title title-up text-center mb-3 font-weight-bold gray">
+                ss
+              </h5>
+              <!-- login fb -->
+              <div v-if="!isOpenMailSignup">
+                <div class="text-center mb-3">
+                  <button class="m-btn mx-auto col-md-6" btn-type="facebook" type="submit">
+                    facebookアカウントで新規登録
+                  </button>
                 </div>
+
+                <div class="mt-2 mb-3 gray text-center">
+                  * SNSに利用状況等が投稿されることはありません
+                </div>
+
+                <div class="or">
+                  <div class="or__block or__block--left"></div>
+                  <div class="or__block or__block--center">
+                    <span>または</span>
+                  </div>
+                  <div class="or__block or__block--right"></div>
+                </div>
+
+                <div class="text-center mb-3">
+                  <button
+                    class="m-btn mx-auto col-md-6" btn-type="emailSignup" type="submit">
+                    メールアドレスで新規登録
+                  </button>
+                </div>
+              </div>
+
+              <!-- email signup -->
+              <div>
+                <h5 class="title title-up text-center mb-3 font-weight-bold gray">
+                  メールアドレスで新規登録
+                </h5>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                  <label>
+                    名前
+                  </label>
+                  <input
+                    name="name"
+                    class="form-control"
+                    type="email"
+                    placeholder="名前を入力してください"
+                    required>
+                </div>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                  <label>
+                    メールアドレス
+                  </label>
+                  <input
+                    name="email"
+                    class="form-control"
+                    type="email"
+                    placeholder="メールアドレスを入力してください"
+                    required>
+                </div>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                   <label>
+                      パスワード
+                    </label>
+                  <input
+                    name="password"
+                    class="form-control"
+                    type="password"
+                    placeholder="パスワードを入力してください"
+                    required>
+                </div>
+                <div class="form-label-group col-lg-8 my-2 mx-auto black font-weight-bold">
+                  <label>
+                    パスワードの確認
+                  </label>
+                  <input
+                    name="password_confirmation" type="password" class="form-control" placeholder="パスワードを入力してください" required>
+                </div>
+                <div class="text-center">
+                  <button
+                    class="m-btn mx-auto col-md-4 my-2" btn-type="primary" type="submit">
+                    メールアドレスで新規登録
+                  </button>
+                </div>
+              </div>
+
+              <div class="text-center gray">
+                  すでにアカウントを持ってる場合は<a href="">ログイン</a>してください
+              </div>
             </div>
+            <div class="footer text-center">
+              <div class="gray">
+                {{ config('app.name') }}の登録は、<a href="/terms" target="_blank">利用規約 </a>および<a href="/privacy" target="_blank">プライバシーポリシー </a>に同意したものとみなします。
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
 </div>
 @endsection
