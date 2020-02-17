b51d6036005c94ae29bf3d297f69b16768ab9d10
RikuHirose <rikuparkour9@gmail.com>
2020-02-17 14:34:54 +0900

    debug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -78,7 +79,6 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        \Log::info($data);
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
