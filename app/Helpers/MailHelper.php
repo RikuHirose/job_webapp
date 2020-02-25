92d854cbfc87f46ac40be794a5e9b7930ab957c4
RikuHirose <rikuparkour9@gmail.com>
2020-02-25 18:59:09 +0900

    fix-typo

--- app/Helpers/MailHelper.php
+++ app/Helpers/MailHelper.php
@@ -9,6 +9,8 @@ class MailHelper
     {
         if (config('app.env') === 'production') {
             Mail::to($to)->send($sendContent);
+
+            $content = view($sendContent->build()->view, $sendContent->build()->viewData)->render();
             \Log::debug('TO: '.$to.' | Subject: '.$sendContent->build()->subject.' | Content: '.$content);
         }
 
